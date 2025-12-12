6a9b3593cd50dcee7133b0a332b3d579abf8d7ca
Nguyen Nam Hoai <nguyennamhoai911@gmail.com>
2025-12-12 20:04:43 +0700

    temp version for recover

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: '/vocabulary/search',
+        destination: '/vocabulary?openSearch=true',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
